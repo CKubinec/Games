a37bd82f3c28faeb3c634323a901f384d28beec4
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 11:54:15 -0600

    Wed Jul 29 11:54:15 MDT 2020 -- FILES_SAVED

--- src/ScoreCard.java
+++ src/ScoreCard.java
@@ -13,24 +13,37 @@ public class ScoreCard {
 
     public void rally(){
         int rallyCount = 1;
+        int winningPlayer;
+        int winningStroke;
         System.out.println("Which player wins rally " + rallyCount +"?");
         System.out.print("1 - Player1\n" +
                         "2-Player2");
-        int selected = scanner.nextInt();
+        winningPlayer = scanner.nextInt();
         boolean properSelection = true;
         while (properSelection)
-        if (selected == 1){
-            System.out.println("How did they win?");
-            System.out.print("");
-        } else if (selected == 2){
-
+        if (winningPlayer == 1){
+            System.out.println("How did Player1 win this rally?");
+            System.out.print("1. Smash\n" +
+                    "2. Slice\n" +
+                    "3. Net\n" +
+                    "4. Drop\n" +
+                    "5. Drive\n");
+            winningStroke = scanner.nextInt();
+        } else if (winningPlayer == 2){
+            System.out.println("How did Player2 win this rally?");
+            System.out.print("1. Smash\n" +
+                    "2. Slice\n" +
+                    "3. Net\n" +
+                    "4. Drop\n" +
+                    "5. Drive\n");
+            winningStroke = scanner.nextInt();
         } else {
             System.out.println("Improper selection");
             properSelection = false;
             System.out.println("Which player wins rally " + rallyCount +"?");
             System.out.print("1 - Player1\n" +
                     "2-Player2");
-            selected = scanner.nextInt();
+            winningPlayer = scanner.nextInt();
         }
     }
 }
