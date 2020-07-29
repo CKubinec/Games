ea32834622f919d630f88f0de60d6dbbce65ca94
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 14:13:24 -0600

    Wed Jul 29 14:13:24 MDT 2020 -- FILES_SAVED

--- src/RockPaperScissorLizardSpock.java
+++ src/RockPaperScissorLizardSpock.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class RockPaperScissorLizardSpock {
 
     public RockPaperScissorLizardSpock() {
@@ -5,6 +7,10 @@ public class RockPaperScissorLizardSpock {
     }
 
     public String getAttack() {
-        
+        int random = ThreadLocalRandom.current().nextInt(1,6);
+
+        switch (random) {
+            case 1:
+        }
     }
 }
