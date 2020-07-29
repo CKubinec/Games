ef725f0aa3cecc71ff165a4ed6c5f5e0b2d4e4d3
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 11:41:34 -0600

    Wed Jul 29 11:41:34 MDT 2020 -- FILES_SAVED

--- src/ScoreCard.java
+++ src/ScoreCard.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ScoreCard {
     private String player1;
     private String player2;
     private ArrayList<String> scoreList;
+    Scanner scanner = new Scanner(System.in);
 
     public ScoreCard(ArrayList<String> scoreList) {
         this.scoreList = scoreList;
