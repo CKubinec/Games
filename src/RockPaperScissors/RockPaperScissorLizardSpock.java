58b7c9bfadfaf4c660cd8ba5c28a01f40472e384
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 15:03:53 -0600

    Wed Jul 29 15:03:53 MDT 2020 -- FILES_SAVED

--- src/RockPaperScissors/RockPaperScissorLizardSpock.java
+++ src/RockPaperScissors/RockPaperScissorLizardSpock.java
@@ -18,8 +18,8 @@ public class RockPaperScissorLizardSpock {
                 noWinner = false;
                 System.out.println("Max Rounds hit!!!");
                 System.out.println("Final Score:");
-                System.out.println("RockPaperScissors.Player 1: " + player1Wins);
-                System.out.println("RockPaperScissors.Player 2: " + player2Wins);
+                System.out.println("Player 1: " + player1Wins);
+                System.out.println("Player 2: " + player2Wins);
                 System.exit(0);
             }
             String player1Attack = player1.getAttack();
@@ -28,19 +28,19 @@ public class RockPaperScissorLizardSpock {
             if (player1Attack.equals("Rock")) {
                 switch (player2Attack) {
                     case "Paper":
-                        System.out.println("RockPaperScissors.Player 2s Paper covers RockPaperScissors.Player 1s Rock");
+                        System.out.println("Player 2s Paper covers Player 1s Rock");
                         player2Wins++;
                         break;
                     case "Lizard":
-                        System.out.println("RockPaperScissors.Player 1s Rock crushes RockPaperScissors.Player 2s Lizard");
+                        System.out.println("Player 1s Rock crushes Player 2s Lizard");
                         player1Wins++;
                         break;
                     case "Scissors":
-                        System.out.println("RockPaperScissors.Player 1s Rock breaks RockPaperScissors.Player 2s Scissors");
+                        System.out.println("Player 1s Rock breaks Player 2s Scissors");
                         player1Wins++;
                         break;
                     case "Spock":
-                        System.out.println("RockPaperScissors.Player 2s Spock vaporizes RockPaperScissors.Player 1s Rock");
+                        System.out.println("Player 2s Spock vaporizes Player 1s Rock");
                         player2Wins++;
                         break;
                     case "Rock":
@@ -53,22 +53,22 @@ public class RockPaperScissorLizardSpock {
             if (player1Attack.equals("Scissors")) {
                 switch (player2Attack) {
                     case "Paper":
-                        System.out.println("RockPaperScissors.Player 1s Scissors cuts RockPaperScissors.Player 2s Paper");
+                        System.out.println("Player 1s Scissors cuts Player 2s Paper");
                         player1Wins++;
                         break;
                     case "Lizard":
-                        System.out.println("RockPaperScissors.Player 1s Scissors decapitates RockPaperScissors.Player 2s Lizard");
+                        System.out.println("Player 1s Scissors decapitates Player 2s Lizard");
                         player1Wins++;
                         break;
                     case "Scissors":
                         System.out.println("DRAW!!");
                         break;
                     case "Spock":
-                        System.out.println("RockPaperScissors.Player 2s Spock smashes RockPaperScissors.Player 1s Scissors");
+                        System.out.println("Player 2s Spock smashes Player 1s Scissors");
                         player2Wins++;
                         break;
                     case "Rock":
-                        System.out.println("RockPaperScissors.Player 2s Rock crushes RockPaperScissors.Player 1s Scissors");
+                        System.out.println("Player 2s Rock crushes Player 1s Scissors");
                         player2Wins++;
                         break;
                     default:
@@ -78,22 +78,22 @@ public class RockPaperScissorLizardSpock {
             if (player1Attack.equals("Spock")) {
                 switch (player2Attack) {
                     case "Paper":
-                        System.out.println("RockPaperScissors.Player 2s Paper disproves RockPaperScissors.Player 1s Spock");
+                        System.out.println("Player 2s Paper disproves Player 1s Spock");
                         player2Wins++;
                         break;
                     case "Lizard":
-                        System.out.println("RockPaperScissors.Player 2s Lizard poisons RockPaperScissors.Player 1s Spock");
+                        System.out.println("Player 2s Lizard poisons Player 1s Spock");
                         player2Wins++;
                         break;
                     case "Scissors":
-                        System.out.println("RockPaperScissors.Player 1s Spock smashes RockPaperScissors.Player 2s Scissors");
+                        System.out.println("Player 1s Spock smashes Player 2s Scissors");
                         player1Wins++;
                         break;
                     case "Spock":
                         System.out.println("DRAW!!");
                         break;
                     case "Rock":
-                        System.out.println("RockPaperScissors.Player 1s Spock vaporizes RockPaperScissors.Player 2s Rock");
+                        System.out.println("Player 1s Spock vaporizes Player 2s Rock");
                         player1Wins++;
                         break;
                     default:
@@ -103,22 +103,22 @@ public class RockPaperScissorLizardSpock {
             if (player1Attack.equals("Lizard")) {
                 switch (player2Attack) {
                     case "Paper":
-                        System.out.println("RockPaperScissors.Player 1s Lizard eats RockPaperScissors.Player 2s Paper");
+                        System.out.println("Player 1s Lizard eats Player 2s Paper");
                         player1Wins++;
                         break;
                     case "Lizard":
                         System.out.println("DRAW!!");
                         break;
                     case "Scissors":
-                        System.out.println("RockPaperScissors.Player 2s Scissors decapitate RockPaperScissors.Player 1s Lizard");
+                        System.out.println("Player 2s Scissors decapitate Player 1s Lizard");
                         player2Wins++;
                         break;
                     case "Spock":
-                        System.out.println("RockPaperScissors.Player 1s Lizard poisons RockPaperScissors.Player 2s Spock");
+                        System.out.println("Player 1s Lizard poisons Player 2s Spock");
                         player1Wins++;
                         break;
                     case "Rock":
-                        System.out.println("RockPaperScissors.Player 2s Rock crushes RockPaperScissors.Player 1s Lizard");
+                        System.out.println("Player 2s Rock crushes Player 1s Lizard");
                         player2Wins++;
                         break;
                     default:
@@ -131,19 +131,19 @@ public class RockPaperScissorLizardSpock {
                         System.out.println("Draw!!");
                         break;
                     case "Lizard":
-                        System.out.println("RockPaperScissors.Player 2s Lizard eats RockPaperScissors.Player 1s Paper");
+                        System.out.println("Player 2s Lizard eats Player 1s Paper");
                         player2Wins++;
                         break;
                     case "Scissors":
-                        System.out.println("RockPaperScissors.Player 2s Scissors cuts RockPaperScissors.Player 1s Paper");
+                        System.out.println("Player 2s Scissors cuts Player 1s Paper");
                         player2Wins++;
                         break;
                     case "Spock":
-                        System.out.println("RockPaperScissors.Player 1s Paper Disproves RockPaperScissors.Player 2s Spock");
+                        System.out.println("Player 1s Paper Disproves Player 2s Spock");
                         player1Wins++;
                         break;
                     case "Rock":
-                        System.out.println("RockPaperScissors.Player 1s Paper covers RockPaperScissors.Player 2s Rock");
+                        System.out.println("Player 1s Paper covers Player 2s Rock");
                         player1Wins++;
                         break;
                     default:
@@ -153,11 +153,11 @@ public class RockPaperScissorLizardSpock {
             rounds++;
             if (player1Wins == 4){
                 noWinner = false;
-                System.out.println("RockPaperScissors.Player 1 Wins!!!!");
+                System.out.println("Player 1 Wins!!!!");
             }
             if (player2Wins == 4){
                 noWinner = false;
-                System.out.println("RockPaperScissors.Player 2 Wins!!!!");
+                System.out.println("Player 2 Wins!!!!");
             }
 
         }
