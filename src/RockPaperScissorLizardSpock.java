ea9b170b4c6d1c4a72de9ff1b59e62828d6bc0b7
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 14:16:21 -0600

    Wed Jul 29 14:16:21 MDT 2020 -- FILES_SAVED

--- src/RockPaperScissorLizardSpock.java
+++ src/RockPaperScissorLizardSpock.java
@@ -3,14 +3,25 @@ import java.util.concurrent.ThreadLocalRandom;
 public class RockPaperScissorLizardSpock {
 
     public RockPaperScissorLizardSpock() {
-
+        
     }
 
     public String getAttack() {
         int random = ThreadLocalRandom.current().nextInt(1,6);
-
+        String attack;
         switch (random) {
-            case 1:
+            case 1: attack = "Rock";
+            break;
+            case 2: attack = "Paper";
+            break;
+            case 3: attack = "Scissors";
+            break;
+            case 4: attack = "Lizard";
+            break;
+            case 5: attack = "Spock";
+            break;
+            default: attack = "Nothing";
         }
+        return attack;
     }
 }
