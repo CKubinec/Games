e9032413b077b1121c2b40752a3255c72f69d440
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 11:40:36 -0600

    Wed Jul 29 11:40:36 MDT 2020 -- FILES_SAVED

--- src/ScoreCard.java
+++ src/ScoreCard.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 
 public class ScoreCard {
+    private String player1;
+    private String player2;
     private ArrayList<String> scoreList;
 
     public ScoreCard(ArrayList<String> scoreList) {
         this.scoreList = scoreList;
     }
 
-    
+    public void rally(){
+        
+    }
 }
