5985b217c3307c8406f8a6ba67fb0d663fb67c46
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 14:44:47 -0600

    Wed Jul 29 14:44:47 MDT 2020 -- FILES_SAVED

--- src/Badminton.java
+++ src/Badminton.java
@@ -1,6 +1,8 @@
 public class Badminton {
     public static void main(String[] args) {
-        ScoreCard scoreCard = new ScoreCard();
-        scoreCard.rally();
+        //ScoreCard scoreCard = new ScoreCard();
+        //scoreCard.rally();
+        RockPaperScissorLizardSpock rps = new RockPaperScissorLizardSpock();
+        rps.play();
     }
 }
