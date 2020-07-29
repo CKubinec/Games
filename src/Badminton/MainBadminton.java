9c15986754870d45b83325740633ff1f7c551188
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 16:37:54 -0600

    Wed Jul 29 16:37:54 MDT 2020 -- FILES_SAVED

--- src/Badminton/MainBadminton.java
+++ src/Badminton/MainBadminton.java
@@ -3,7 +3,7 @@ package Badminton;
 public class MainBadminton {
 
     public static void main(String[] args) {
-        //Tests that classes are working properly
+        //Tests the Badminton game w/ scorecard.
         ScoreCard scoreCard = new ScoreCard();
         Badminton badminton = new Badminton(scoreCard);
         badminton.rally();
--- src/RockPaperScissors/MainRPS.java
+++ src/RockPaperScissors/MainRPS.java
@@ -2,6 +2,7 @@ package RockPaperScissors;
 
 public class MainRPS {
     public static void main(String[] args) {
+        //Tests the RockPaperScissorLizardSpock game
         Player player1 = new Player();
         Player player2 = new Player();
         RockPaperScissorLizardSpock rockPaperScissorLizardSpock =  new RockPaperScissorLizardSpock(player1, player2);
