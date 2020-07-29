47fe6e9c8b314a5e58ef1810343dc85b385e6bce
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 11:49:55 -0600

    Wed Jul 29 11:49:55 MDT 2020 -- FILES_SAVED

--- src/ScoreCard.java
+++ src/ScoreCard.java
@@ -13,7 +13,24 @@ public class ScoreCard {
 
     public void rally(){
         int rallyCount = 1;
-        System.out.print("Which player wins rally " + rallyCount);
-        scanner.nextLine();
+        System.out.println("Which player wins rally " + rallyCount +"?");
+        System.out.print("1 - Player1\n" +
+                        "2-Player2");
+        int selected = scanner.nextInt();
+        boolean properSelection = true;
+        while (properSelection)
+        if (selected == 1){
+            System.out.println("How did they win?");
+            System.out.print("");
+        } else if (selected == 2){
+
+        } else {
+            System.out.println("Improper selection");
+            properSelection = false;
+            System.out.println("Which player wins rally " + rallyCount +"?");
+            System.out.print("1 - Player1\n" +
+                    "2-Player2");
+            selected = scanner.nextInt();
+        }
     }
 }
