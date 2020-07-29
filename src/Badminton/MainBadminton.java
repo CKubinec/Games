c1b41f10395b92774ea9b7da29c077099e7bf913
e830c0cd-2186-4e44-8c11-981646960cc7 <ckubinec1@athabasca.edu>
2020-07-29 16:44:02 -0600

    Wed Jul 29 16:44:02 MDT 2020 -- FILES_SAVED

--- src/Badminton/MainBadminton.java
+++ src/Badminton/MainBadminton.java
@@ -3,7 +3,7 @@ package Badminton;
 public class MainBadminton {
 
     public static void main(String[] args) {
-        //Tests the Badminton game w/ scorecard.
+        //Tests the Badminton game w/ scorecard. *USER ENTERS DATA*
         ScoreCard scoreCard = new ScoreCard();
         Badminton badminton = new Badminton(scoreCard);
         badminton.rally();
